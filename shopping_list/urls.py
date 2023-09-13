fcea1a41de405b7b1fd5e149fb53da9943817b8b
Gabriella Naomi Hutagalung <gabriella.naomi21@ui.ac.id>
2023-09-13 14:29:44 +0700

    Tutorial 2 Form and Data Delivery

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
 from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/',include('main.urls'))
+    path('',include('main.urls')),
+    path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
 ]
